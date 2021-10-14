bdf1cd05c0a8d16b602363299b9574903ea0eca2
Frank Pagan <frank@cocreate.app>
2021-10-14 00:07:31 +0000

    feat: Initial Release

---
+++ README.md
@@ -0,0 +1,84 @@
+# CoCreate-signup-animation
+
+A simple signup-animation component in vanilla javascript. Easily configured using HTML5 data-attributes and/or JavaScript API. Take it for a spin in our [playground!](https://cocreate.app/docs/signup-animation)
+
+![min file size in bytes](https://img.badgesize.io/https://cdn.cocreate.app/signup-animation/latest/CoCreate-signup-animation.min.js?style=flat-square&label=minified&color=orange)
+![gzip file size in bytes](https://img.badgesize.io/https://cdn.cocreate.app/signup-animation/latest/CoCreate-signup-animation.min.js?compression=gzip&style=flat-square&label=gzip&color=yellow)
+![brotlifile size in bytes](https://img.badgesize.io/https://cdn.cocreate.app/signup-animation/latest/CoCreate-signup-animation.min.js?compression=brotli&style=flat-square&label=brotli)
+![GitHub latest release](https://img.shields.io/github/v/release/CoCreate-app/CoCreate-signup-animation?style=flat-square)
+![GitHub](https://img.shields.io/github/license/CoCreate-app/CoCreate-signup-animation?style=flat-square)
+![GitHub](https://img.shields.io/static/v1?style=flat-square&label=&message=Hiring&color=blueviolet)
+
+
+![CoCreate-signup-animation](https://cdn.cocreate.app/docs/CoCreate-signup-animation.gif)
+
+## [Docs & Demo](https://cocreate.app/docs/signup-animation)
+
+For a complete guide and working demo refer to the [doumentation](https://cocreate.app/docs/signup-animation)
+
+## CDN
+
+```html
+<script src="https://cdn.cocreate.app/signup-animation/latest/CoCreate-signup-animation.min.js"></script>
+```
+
+```html
+<script src="https://cdn.cocreate.app/signup-animation/latest/CoCreate-signup-animation.min.css"></script>
+```
+
+## NPM
+
+```shell
+$ npm i @cocreate/signup-animation
+```
+
+## yarn
+
+```shell
+$ yarn install @cocreate/signup-animation
+```
+
+# Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [Announcements](#announcements)
+- [Roadmap](#roadmap)
+- [How to Contribute](#how-to-contribute)
+- [About](#about)
+- [License](#license)
+
+<a name="announcements"></a>
+
+# Announcements
+
+All updates to this library are documented in our [CHANGELOG](https://github.com/CoCreate-app/CoCreate-signup-animation/blob/master/CHANGELOG.md) and [releases](https://github.com/CoCreate-app/CoCreate-signup-animation/releases). You may also subscribe to email for releases and breaking changes.
+
+<a name="roadmap"></a>
+
+# Roadmap
+
+If you are interested in the future direction of this project, please take a look at our open [issues](https://github.com/CoCreate-app/CoCreate-signup-animation/issues) and [pull requests](https://github.com/CoCreate-app/CoCreate-signup-animation/pulls). We would love to hear your feedback.
+
+<a name="about"></a>
+
+# About
+
+CoCreate-signup-animation is guided and supported by the CoCreate Developer Experience Team.
+
+Please Email the Developer Experience Team [here](mailto:develop@cocreate.app) in case of any queries.
+
+CoCreate-signup-animation is maintained and funded by CoCreate. The names and logos for CoCreate are trademarks of CoCreate, LLC.
+
+<a name="contribute"></a>
+
+# How to Contribute
+
+We encourage contribution to our libraries (you might even score some nifty swag), please see our [CONTRIBUTING](https://github.com/CoCreate-app/CoCreate-signup-animation/blob/master/CONTRIBUTING.md) guide for details.
+
+We want this library to be community-driven, and CoCreate led. We need your help to realize this goal. To help make sure we are building the right things in the right order, we ask that you create [issues](https://github.com/CoCreate-app/CoCreate-signup-animation/issues) and [pull requests](https://github.com/CoCreate-app/CoCreate-signup-animation/pulls) or merely upvote or comment on existing issues or pull requests.
+
+We appreciate your continued support, thank you!
+
+# License
+
+[The MIT License (MIT)](https://github.com/CoCreate-app/CoCreate-signup-animation/blob/master/LICENSE)
---
+++ src/index.js
@@ -0,0 +1,202 @@
+import './css/common.css';
+import './css/hamburger.css';
+import './css/meatballs.css';
+import './css/sushi.css';
+import './css/kebab.css';
+import './css/pizza.css';
+
+function init(){
+
+let animation = document.querySelector(".animation-wrapper");
+let button = document.querySelector(".startAnimation");
+
+animation.innerHTML = animationHtml;
+let hamburgerEl = document.querySelector("#hamburger");
+let meatballEl = document.querySelector("#meatballs");
+let sushiEl = document.querySelector("#sushi");
+let kebabEl = document.querySelector("#kebab");
+let pizzaEl = document.querySelector("#pizza");
+
+button.addEventListener("click", function() {
+    setHamburger();
+    hamburgerEl.classList.add('hamburger');
+    hamburgerEl.classList.remove('animationHide');
+});
+
+function setHamburger() {
+    setTimeout(function() { 
+        setMeatballs();
+        hamburgerEl.classList.add('animationHide');
+        meatballEl.classList.add('meatballs');
+        meatballEl.classList.remove('animationHide');
+    }, 3000);
+}
+function setMeatballs() {
+    setTimeout(function() { 
+        setSushi() ;
+        meatballEl.classList.add('animationHide');
+        sushiEl.classList.add('sushi');
+        sushiEl.classList.remove('animationHide');
+    }, 3000);
+}
+function setSushi() {
+    setTimeout(function() { 
+        setKebab();
+        sushiEl.classList.add('animationHide');
+        kebabEl.classList.add('kebab');
+        kebabEl.classList.remove('animationHide');
+    }, 3000);
+} 
+function setKebab() {
+    setTimeout(function() { 
+        setPizza();
+        kebabEl.classList.add('animationHide');
+        pizzaEl.classList.add('pizza');
+        pizzaEl.classList.remove('animationHide');
+    }, 3000);
+}
+
+function setPizza() {
+    setTimeout(function() { 
+        setHamburger();
+        pizzaEl.classList.add('animationHide');
+        // hamburgerEl.classList.add('sushi')
+        hamburgerEl.classList.remove('animationHide');
+    }, 3000);
+}
+
+
+}
+
+const animationHtml = `
+    <div id="hamburger" class="wrapper animationHide">
+        <div class="icon">
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="title"></span>
+        </div>
+        <div class="icon">
+            <span class="heading"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="image"></span>
+        </div>
+        <div class="icon">
+            <span class="action"></span>
+            <span class="action"></span>
+            <span class="action"></span>
+        </div>
+    </div>
+
+    <div id="sushi" class="wrapper animationHide">
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon">
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+
+            <span class="tab"></span>
+            <span class="tab"></span>
+            <span class="tab"></span>
+
+            <span class="action"></span>
+        </div>
+        <div class="icon"></div>
+        <div class="icon">
+            <span class="heading"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+        </div>
+        <div class="icon"></div>
+        <div class="icon">
+            <span class="image"></span>
+            <span class="image"></span>
+        </div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+    </div>
+
+    <div id="meatballs" class="wrapper animationHide">
+        <div class="icon">
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+        </div>
+        <div class="icon">
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+
+            <span class="title"></span>
+
+            <span class="image"></span>
+            <span class="image"></span>
+            <span class="image"></span>
+
+            <span class="image"></span>
+            <span class="image"></span>
+            <span class="image"></span>
+        </div>
+        <div class="icon">
+            <span class="item"></span>
+            <span class="item"></span>
+        </div>
+    </div>
+
+    <div id="kebab" class="wrapper animationHide">
+        <div class="icon">
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="image"></span>
+            <span class="image"></span>
+            <span class="image"></span>
+        </div>
+        <div class="icon">
+            <span class="title"></span>
+            <span class="item"></span>
+            <span class="item"></span>
+            <span class="item"></span>
+            <span class="item"></span>
+            <span class="item"></span>
+            <span class="item"></span>
+        </div>
+        <div class="icon">
+            <span class="detail"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+        </div>
+    </div>
+    
+    <div id="pizza" class="wrapper animationHide">
+        <div class="icon">
+            <span class="hor"></span>
+            <span class="ver"></span>
+        </div>
+        <div class="icon">
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="traffic"></span>
+            <span class="title"></span>
+            <span class="heading"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="txtline"></span>
+            <span class="image"></span>
+        </div>
+        <div class="icon">
+            <span class="tab"></span>
+            <span class="tab"></span>
+            <span class="tab"></span>
+        </div>
+    </div>
+`;
+
+
+init();
+export default {init};
