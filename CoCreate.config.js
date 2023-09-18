71883a23884e804703f7b496be989836aabe1145
frankpagan <frank@cocreate.app>
2023-09-17 19:11:36 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "637ca66650234ef1671ce333",
                 "name": "index.html",
-                "path": "/docs/signup-animation/index.html",
+                "path": "/docs/signup-animation",
+                "pathname": "/docs/signup-animation/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/signup-animation",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "signup-animation",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
